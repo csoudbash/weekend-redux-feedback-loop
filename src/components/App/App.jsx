e97220ed135272545ecc80f5de6b940a59c7b0ca
Cameron Soudbash <csoudbash@gmail.com>
2022-02-26 14:24:20 -0600

    finished file structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className='App'>
+<>
+
+
+<div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
     </div>
+
+      <Router>
+        <div className='App'>
+        <Header/>
+
+          <img src='images/pizza_photo.png' />
+          <p>Pizza is great.</p>
+          {/* <Route path="/" exact>
+            <Home />
+          </Route>  */}
+          <Route path="/pizza-options">
+            <PizzaOptions />
+          </Route>
+          <Route path="/customer-info">
+            <CustomerInfo />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/administrator">
+            <Administrator />
+          </Route>
+        </div>
+      </Router>
+
+
+  </>  
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import { logger } from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// REDUCERS!!! 
+const 
+
+// END REDUCERS
+
+//STORE
+const storeInstance = createStore(
+    combineReducers(
+        {
+        
+        }
+    ),
+    applyMiddleware(
+        logger
+    )
+
+);
+// END STORE
+
+ReactDOM.render(
+<Provider store= {storeInstance}>
+    <App />
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
